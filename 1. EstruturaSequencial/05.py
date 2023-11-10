158dcdf09117965763d5526ed62cd6fb7790ee5d
Leonardo Gironde de Gusmão <95931762+leoggusmao@users.noreply.github.com>
2023-11-10 20:48:05 -0300

    Add files via upload

---
+++ 1. EstruturaSequencial/05.py
@@ -0,0 +1,5 @@
+#Faça um Programa que converta metros para centímetros.
+
+metros = float(input('Informe a quantidade de metros: '))
+centimetros = metros * 100
+print(f'Esta quantidade corresponde a {centimetros:,.0f} centímetros.')
